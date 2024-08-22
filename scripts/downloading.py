6a5da7e44b5d41a588c399dc66b9c098953fc802
TangHanshi <122192395+TangHanshi@users.noreply.github.com>
2024-08-22 18:04:44 +1000

    update

--- scripts/downloading.py
+++ scripts/downloading.py
@@ -6,9 +6,17 @@ Download data from the cloudfront server and save it to the landing folder
 """
 
 # import libraries
+from pyspark.sql import functions as F
 from pyspark.sql import SparkSession
-import requests
+from pyspark.sql.functions import *
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sbs
+import geopandas as gpd
+import folium
 import os
+import requests
 
 # Create a spark session (which will run spark jobs)
 spark = (
@@ -37,8 +45,11 @@ for folder in data_folders:
         os.makedirs(path)
         print(f'Created folder: {path}')
         
+# ==============================================================================
+# TLC Datasource Download
+# ==============================================================================
 
-
+# Download the 2023 yellow trip data from the cloudfront server
 URL_TEMPLATE = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_"
 YEAR = ['2023']
 MONTH = range(5, 13)
@@ -58,25 +69,7 @@ for year in YEAR:
         print(f'Finished download for {year}-{month_str}')
 
 
-URL_TEMPLATE = "https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_"
-YEAR = ['2023']
-MONTH = range(5, 13)
-output_relative_dir = '../data/landing/tlc_data'
-
-for year in YEAR:
-    for month in MONTH:
-        print(f'Starting download for {year}-{str(month).zfill(2)}')
-        month_str = str(month).zfill(2)
-        url = f'{URL_TEMPLATE}{year}-{month_str}.parquet'
-        output_dir = f"{output_relative_dir}/{year}/G-{year}-{month_str}.parquet"
-        
-        response = requests.get(url, verify=True)
-        with open(output_dir, 'wb') as file:
-            file.write(response.content)
-            
-        print(f'Finished download for {year}-{month_str}')
-
-
+# Download the 2024 yellow trip data from the cloudfront server
 URL_TEMPLATE = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_"
 YEAR = ['2024']
 MONTH = range(1, 6)
@@ -95,6 +88,27 @@ for year in YEAR:
             
         print(f'Finished download for {year}-{month_str}')
 
+
+# Download the 2023 green trip data from the cloudfront server
+URL_TEMPLATE = "https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_"
+YEAR = ['2023']
+MONTH = range(5, 13)
+output_relative_dir = '../data/landing/tlc_data'
+
+for year in YEAR:
+    for month in MONTH:
+        print(f'Starting download for {year}-{str(month).zfill(2)}')
+        month_str = str(month).zfill(2)
+        url = f'{URL_TEMPLATE}{year}-{month_str}.parquet'
+        output_dir = f"{output_relative_dir}/{year}/G-{year}-{month_str}.parquet"
+        
+        response = requests.get(url, verify=True)
+        with open(output_dir, 'wb') as file:
+            file.write(response.content)
+            
+        print(f'Finished download for {year}-{month_str}')
+
+# Download the 2024 green trip data from the cloudfront server
 URL_TEMPLATE = "https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_"
 YEAR = ['2024']
 MONTH = range(1, 6)
@@ -113,6 +127,66 @@ for year in YEAR:
             
         print(f'Finished download for {year}-{month_str}')
 
+# ==============================================================================
+# Download weather data from Central Park 
+# ==============================================================================
+
+import requests
+# URL of the CSV file
+url = 'https://www.ncei.noaa.gov/access/past-weather/USW00094728/data.csv'
+
+# Directory where the file will be saved
+output_relative_dir = '../data/landing/external'
+output_file_path = os.path.join(output_relative_dir, 'NYC weather.csv')
+
+# Create the directory if it does not exist
+os.makedirs(output_relative_dir, exist_ok=True)
+
+# Send a GET request to fetch the file
+response = requests.get(url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Save the content to a file
+    with open(output_file_path, 'wb') as file:
+        file.write(response.content)
+    
+    # Read the saved CSV file into a pandas DataFrame
+    df = pd.read_csv(output_file_path)
+    # Display the first few rows of the DataFrame
+    print(df.head())
+else:
+    print(f"Failed to download file. Status code: {response.status_code}")
+
+# ==============================================================================
+# Download traffic data from NYC Open Data
+# ==============================================================================
+
+# URL of the CSV file with limit parameter
+url = 'https://data.cityofnewyork.us/resource/btm5-ppia.csv?$limit=45000'
+
+# Directory where the file will be saved
+output_relative_dir = '../data/landing/external'
+output_file_path = os.path.join(output_relative_dir, 'NYC Traffic.csv')
+
+# Create the directory if it does not exist
+os.makedirs(output_relative_dir, exist_ok=True)
+
+# Send a GET request to fetch the file
+response = requests.get(url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Save the content to a file
+    with open(output_file_path, 'wb') as file:
+        file.write(response.content)
+    
+    # Read the saved CSV file into a pandas DataFrame
+    df = pd.read_csv(output_file_path)
+    # Display the first few rows of the DataFrame
+    print(df.head())
+else:
+    print(f"Failed to download file. Status code: {response.status_code}")
 
 
 
