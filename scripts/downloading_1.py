f87d6427de21a16c0b1c9e849c10c04cfff8309f
TangHanshi <122192395+TangHanshi@users.noreply.github.com>
2024-08-26 01:55:59 +1000

    /

--- scripts/downloading_1.py
+++ scripts/downloading_1.py
@@ -57,32 +57,3 @@ download_tlc_data(2024, 1, 5, color='yellow')
 download_tlc_data(2023, 6, 12, color='green')
 download_tlc_data(2024, 1, 5, color='green')
 
-# Function to download external data
-def download_external_data(url, output_file_path):
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        with open(output_file_path, 'wb') as file:
-            file.write(response.content)
-        print(f"Downloaded and saved file to {output_file_path}")
-        
-        # Read and display the data if it is a CSV
-        if output_file_path.endswith('.csv'):
-            df = pd.read_csv(output_file_path)
-            print(df.head())
-    else:
-        print(f"Failed to download file. Status code: {response.status_code}")
-
-# Download weather data from Central Park NYC
-weather_url_2023 = 'https://www.ncei.noaa.gov/data/global-hourly/access/2023/72505394728.csv'
-weather_url_2024 = 'https://www.ncei.noaa.gov/data/global-hourly/access/2024/72505394728.csv'
-weather_output_path_2023 = './data/landing/external/NYC_weather_2023.csv'
-weather_output_path_2024 = './data/landing/external/NYC_weather_2024.csv'
-download_external_data(weather_url_2023, weather_output_path_2023)
-download_external_data(weather_url_2024, weather_output_path_2024)
-
-# Download traffic data from NYC Open Data
-traffic_url = 'https://data.cityofnewyork.us/resource/btm5-ppia.csv?$limit=45000'
-traffic_output_path = './data/landing/external/NYC_Traffic.csv'
-download_external_data(traffic_url, traffic_output_path)
- 
---
+++ scripts/downloading_2&3.py
@@ -0,0 +1,34 @@
+# Import necessary libraries
+from pyspark.sql import SparkSession
+import os
+import requests
+import pandas as pd
+
+# Function to download external data
+def download_external_data(url, output_file_path):
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        with open(output_file_path, 'wb') as file:
+            file.write(response.content)
+        print(f"Downloaded and saved file to {output_file_path}")
+        
+        # Read and display the data if it is a CSV
+        if output_file_path.endswith('.csv'):
+            df = pd.read_csv(output_file_path)
+            print(df.head())
+    else:
+        print(f"Failed to download file. Status code: {response.status_code}")
+
+# Download weather data from Central Park NYC
+weather_url_2023 = 'https://www.ncei.noaa.gov/data/global-hourly/access/2023/72505394728.csv'
+weather_url_2024 = 'https://www.ncei.noaa.gov/data/global-hourly/access/2024/72505394728.csv'
+weather_output_path_2023 = './data/landing/external/NYC_weather_2023.csv'
+weather_output_path_2024 = './data/landing/external/NYC_weather_2024.csv'
+download_external_data(weather_url_2023, weather_output_path_2023)
+download_external_data(weather_url_2024, weather_output_path_2024)
+
+# Download event data from NYC Open Data
+base_url = 'https://data.cityofnewyork.us/resource/tvpp-9vvx.csv?$limit=30000'
+event_output_path = './data/landing/external/NYC_Event.csv'
+download_external_data(base_url, event_output_path)
